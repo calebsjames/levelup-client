1dea5116dafdcd13fcd93b0f68ab54c231dfa452
Caleb James <calebsjames@gmail.com>
2021-05-07 11:56:54 -0500

    feat: users can joiin and leave games

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,13 +1,43 @@
 import React from "react"
 import { Route } from "react-router-dom"
+import { EventForm } from "./game/EventForm.js"
+import { EventList } from "./game/EventList.js"
+import { EventProvider } from "./game/EventProvider.js"
+import { GameForm } from "./game/GameForm.js"
+import { GameList } from "./game/GameList.js"
+import { GameProvider } from "./game/GameProvider.js"
+
+
 
 export const ApplicationViews = () => {
     return <>
         <main style={{
             margin: "5rem 2rem",
-            backgroundColor: "lightgoldenrodyellow"
+            lineHeight: "1.75rem"
         }}>
-            Application views
+            <GameProvider>           
+                    <Route exact path="/">
+                        <GameList />
+                    </Route>
+
+                    <Route exact path="/games/new">
+                        <GameForm />
+                    </Route>
+            </GameProvider>
+            
+            <EventProvider>
+                <Route exact path="/events">
+                    <EventList />
+                </Route>
+            </EventProvider>
+            <EventProvider>
+                <GameProvider>
+                    <Route exact path="/events/new">
+                        <EventForm />
+                    </Route>
+                </GameProvider>
+            </EventProvider>
+
         </main>
     </>
 }
